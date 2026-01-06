19f4eb621c9687a6d34430ba946286873b66d074
Cr4nkSt4r <github.gibberish503@passmail.com>
2026-01-06 23:18:51 +0100

    feat(utils): add log functionality

---
+++ src/utils/log.cpp
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2026 Cr4nkSt4r - https://github.com/Cr4nkSt4r/Borderlands-4.NcsParser
+ */
+#include "log.h"
+#include <cstdio>
+
+
+void vprint(FILE *f, const char *prefix, const char *fmt, va_list args) {
+  std::fputs(prefix, f);
+  std::vfprintf(f, fmt, args);
+  std::fputc('\n', f);
+  std::fflush(f);
+}
+
+namespace bl4::log {
+  void info(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprint(stdout, "", fmt, args);
+    va_end(args);
+  }
+
+  void error(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprint(stderr, "[ERROR] ", fmt, args);
+    va_end(args);
+  }
+}
---
+++ src/utils/log.h
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2026 Cr4nkSt4r - https://github.com/Cr4nkSt4r/Borderlands-4.NcsParser
+ */
+#pragma once
+
+#include <cstdarg>
+
+
+namespace bl4::log {
+  void info(const char *fmt, ...);
+  void error(const char *fmt, ...);
+}
+
+#define BL4_LOG_INFO(fmt, ...) ::bl4::log::info(fmt, ##__VA_ARGS__)
+#define BL4_LOG_ERROR(fmt, ...) ::bl4::log::error(fmt, ##__VA_ARGS__)
