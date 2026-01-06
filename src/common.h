ed7540833dbaab13805c9506d493235d8e87fa78
Cr4nkSt4r <github.gibberish503@passmail.com>
2026-01-06 23:21:47 +0100

    feat(main): add common header

---
+++ src/common.h
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2026 Cr4nkSt4r - https://github.com/Cr4nkSt4r/Borderlands-4.NcsParser
+ */
+#pragma once
+
+#if !defined(NDEBUG) && !defined(_DEBUG)
+#define _DEBUG
+#endif
+
+#include "utils/fs_utils.h"
+#include "utils/log.h"
+#include "oodle/oodle_api.h"
